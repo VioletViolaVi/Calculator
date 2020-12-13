bae61c93c34b77bb6ce0041d7faa422d2b3c0ef1
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-13 21:22:12 +0000

    Add click function to buttons.

--- assets/scripts.js
+++ assets/scripts.js
@@ -33,12 +33,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // removes commas from formatted number
     function removeCommasFromNumbers(answer) {
-        return Number(answer.replace(/,/g,""));
+        return Number(answer.replace(/,/g, ""));
     };
 
-    
+    let operators = document.querySelectorAll(".operator");
+    for (let operatorIndex = 0; operatorIndex < operators.length; operatorIndex++) {
+        operators[operatorIndex].addEventListener("click", function () {
+        });
+    };
 
-
+    let numbers = document.querySelectorAll(".number");
+    for (let numberIndex = 0; numberIndex < numbers.length; numberIndex++) {
+        numbers[numberIndex].addEventListener("click", function () {
+            let answer = removeCommasFromNumbers(getAnswer);
+            if (answer != NaN) { // if answer is a number
+                answer = answer + numbers[numberIndex];
+                displayAnswer(answer);
+            };
+        });
+    };
 
 
 
