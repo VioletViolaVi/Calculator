946715e60935b04054216e876fd7478566c285db
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-13 19:18:54 +0000

    Add formatted values to calculator screen.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,30 +1,27 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
 
-    console.log("Hello World");
+    // collects calculation history from html
+    function getHistory() {
+        return document.querySelector("#history").innerText;
+    };
 
+    // shows calculation history in screen
+    function displayHistory(history) {
+        document.querySelector("#history").innerText = history;
+    };
 
+    // shows answer in screen
+    function displayAnswer(answer) {
+        document.querySelector("#answer").innerText = addCommasToAnswer(answer);
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // adds commas to number in screen
+    function addCommasToAnswer(answer) {
+        let n = Number(answer);
+        let formattedAnswer = n.toLocaleString("en");
+        return formattedAnswer;
+    };
+    displayAnswer(22222222);
 
 
 
