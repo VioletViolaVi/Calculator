4c21892c600c484c3b54db9169543dddec10b9ab
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-15 17:28:46 +0000

    Clear comments and format code.

--- assets/scripts.js
+++ assets/scripts.js
@@ -58,11 +58,11 @@ $("document").ready(function () {
     };
     secondBracket();
 
-    // adds 2nd bracket(s) if only 1st bracket(s) is/are present
+    // adds matching 2nd bracket(s) if only 1st bracket(s) is/are present
     function autoAddClosingBrackets() {
         $("#equals").click(function () {
             let screenContent = $("#answer").text();
-            let firstBrackets = screenContent.match(/\(/g);            
+            let firstBrackets = screenContent.match(/\(/g);
             if ($("#answer").text().includes("(") && !$("#answer").text().includes(")")) {
                 firstBrackets.forEach(function () {
                     $("#history").append(")");
@@ -101,145 +101,4 @@ $("document").ready(function () {
         });
     };
     performsArithmetic();
-
-    // shows error message 
-    function errorMessage() {
-        $("#equals").click(function () {
-            if ("SyntaxError") {
-                $("#answer").text("ERROR");
-            };
-        });
-    };
-    errorMessage();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // collects calculation history from html
-//     function getHistory() {
-//         return document.querySelector("#history").innerText;
-//     };
-
-//     // shows calculation history in screen
-//     function displayHistory(history) {
-//         document.querySelector("#history").innerText = history;
-//     };
-
-//     // collects calculation answer from html
-//     function getAnswer() {
-//         return document.querySelector("#answer").innerText;
-//     };
-
-//     // shows answer in screen
-//     function displayAnswer(answer) {
-//         if (answer === "") {
-//             document.querySelector("#answer").innerText = 0;
-//         } else {
-//             document.querySelector("#answer").innerText = addCommasToAnswer(answer);
-//         }
-//     };
-
-//     // adds commas to number
-//     function addCommasToAnswer(answer) {
-//         let number = Number(answer);
-//         let formattedAnswer = number.toLocaleString("en");
-//         return formattedAnswer;
-//     };
-
-//     // removes commas from formatted number
-//     function removeCommasFromNumbers(answer) {
-//         return Number(answer.replace(/,/g, ""));
-//     };
-
-//     let operators = document.querySelectorAll(".operator");
-//     for (let operatorIndex = 0; operatorIndex < operators.length; operatorIndex++) {
-//         operators[operatorIndex].addEventListener("click", function () {
-//         });
-//     };
-
-//     let numbers = document.querySelectorAll(".number");
-//     for (let numberIndex = 0; numberIndex < numbers.length; numberIndex++) {
-//         numbers[numberIndex].addEventListener("click", function () {
-//             let answer = removeCommasFromNumbers(getAnswer);
-//             if (answer != NaN) { // if answer is a number
-//                 answer = answer + numbers[numberIndex];
-//                 displayAnswer(answer);
-//             };
-//         });
-//     };
-// });
-
-
-
-    // $("#equals").click(function () {
-    //     let answerText = $("#answer").text();
-    //     for (let index = 0; index < answerText.length; index++) {
-    //         console.log(index);
-    //         if ($("#answer").text().includes("(") && !$("#answer").text().includes(")")) {
-    //             $("#answer").append(")");
-    //         };
-    //     };
-    // });
-
-    //     let firstBracketAmount = ;
-    //     let all1stBrackets = [];
-    //     for (let index = 0; index < #.length; index++) {
-    //         if ( $("#answer").text().includes("(") ) {
-    //             all1stBrackets.push(")");
-    //             console.log(all1stBrackets);
-    //         };
-    //     };
-    // });
-    // all1stBrackets.forEach(element => {
-
-    // });
-    // if ($("#answer").text("(").length !== $("#answer").text(")").length) {
-    //     $("#answer").append(")");
-    // };
